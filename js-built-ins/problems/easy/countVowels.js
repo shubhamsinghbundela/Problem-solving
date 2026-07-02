36a873e3c84169f6a75900b335f43578b0c61207
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-07-02 12:41:47 +0530

    countVowels

--- js-built-ins/problems/easy/countVowels.js
+++ js-built-ins/problems/easy/countVowels.js
@@ -27,9 +27,24 @@
 
 function countVowels(str) {
   // Your code here
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] == "a" ||
+      str[i] == "e" ||
+      str[i] == "i" ||
+      str[i] == "o" ||
+      str[i] == "u" ||
+      str[i] == "A" ||
+      str[i] == "E" ||
+      str[i] == "I" ||
+      str[i] == "O" ||
+      str[i] == "U"
+    ) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = { countVowels };
-
-
-
