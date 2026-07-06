0ac55578c9db346153a075e604eeea1298a39620
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-07-06 18:55:33 +0530

    done promiseAll problem

--- Asynchronous-Javascript/promises/easy/promiseAll.js
+++ Asynchronous-Javascript/promises/easy/promiseAll.js
@@ -3,6 +3,8 @@
 // You are required to implement your own version of Promise.all without using the built-in method.
 // The function should accept an array of values that may include Promises or plain constants.
 // It must resolve with an array of results in the same order once all inputs resolve, or reject immediately if any input rejects.
+
+// 1st solution
 async function promiseAll(promises) {
   return new Promise((res, rej) => {
     let results = [];
@@ -22,6 +24,38 @@ async function promiseAll(promises) {
         })
         .catch(rej);
     });
+  });
+}
+
+// 2nd solution
+async function promiseAll(promises) {
+  return new Promise((res, rej) => {
+    let index = 0;
+    let results = [];
+    let completed = 0;
+    if (!Array.isArray(promises)) {
+      rej(new TypeError("Input must be an array"));
+    }
+    function next() {
+      if (completed === promises.length) {
+        // console.log(completed);
+        res(results);
+      }
+      while (index < promises.length) {
+        let currentIndex = index;
+        index += 1;
+        Promise.resolve(promises[currentIndex])
+          .then((data) => {
+            results[currentIndex] = data;
+            completed += 1;
+            next();
+          })
+          .catch((err) => {
+            rej(err);
+          });
+      }
+    }
+    next();
   });
 }
 module.exports = promiseAll;
--- Asynchronous-Javascript/rough.js
+++ Asynchronous-Javascript/rough.js
@@ -1,37 +1,38 @@
 async function promiseAll(promises) {
-  let index = 0;
-  let results = [];
-  let completed = 0;
   return new Promise((res, rej) => {
+    let index = 0;
+    let results = [];
+    let completed = 0;
     function next() {
       if (completed === promises.length) {
         // console.log(completed);
         res(results);
       }
-      let currentIndex = index;
-      index += 1;
-      Promise.resolve(promises[currentIndex])
-        .then((data) => {
-          results[currentIndex] = data;
-          completed += 1;
-          next();
-        })
-        .catch((err) => {
-          rej(err);
-        });
+      while (index < promises.length) {
+        let currentIndex = index;
+        index += 1;
+        Promise.resolve(promises[currentIndex])
+          .then((data) => {
+            results[currentIndex] = data;
+            completed += 1;
+            next();
+          })
+          .catch((err) => {
+            rej(err);
+          });
+      }
     }
     next();
   });
 }
 
 (async () => {
-  const p1 = Promise.resolve(1);
-  const p2 = Promise.reject("error");
-  const p3 = Promise.resolve(3);
-  const results = await promiseAll([p1, p2, p3]);
-  console.log(results);
+  const p1 = new Promise((res) => setTimeout(() => res(1), 100));
+  const p2 = new Promise((res) => setTimeout(() => res(2), 50));
+  const p3 = new Promise((res) => setTimeout(() => res(3), 10));
 
-  // await expect(promiseAll([p1, p2, p3])).rejects.toBe("error");
+  const result = await promiseAll([p1, p2, p3]);
+  console.log(result);
 })();
 
 // expect(result).toEqual([1, 2, 3]);
