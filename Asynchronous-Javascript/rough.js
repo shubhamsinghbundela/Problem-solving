8789b5b04e328818a75a9f10c2f71ad83096dc37
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-07-06 15:39:30 +0530

    done with problem doubleTry

--- Asynchronous-Javascript/rough.js
+++ Asynchronous-Javascript/rough.js
@@ -1,15 +1,28 @@
-function delayResult(value, ms) {
-  return new Promise((res) => setTimeout(res(value), ms));
+async function doubleTry(fn) {
+  try {
+    return await fn();
+  } catch (err) {
+    return await fn();
+  }
 }
 
+let callCount = 0;
+const fn = async () => {
+  callCount++;
+  return "Success";
+};
+
 (async () => {
-  const start = Date.now();
-  const result = await delayResult("Success", 100);
-  const end = Date.now();
+  let callCount = 0;
+  const fn = async () => {
+    callCount++;
+    if (callCount === 1) throw new Error("First Failure");
+    return "Second Success";
+  };
 
-  console.log("result", result);
-  console.log(end - start);
+  const result = await doubleTry(fn);
+  console.log(result);
+  console.log(callCount);
+  // expect(result).toBe("Second Success");
+  // expect(callCount).toBe(2);
 })();
-
-// expect(result).toBe("Success");
-// expect(end - start).toBeGreaterThanOrEqual(100);
