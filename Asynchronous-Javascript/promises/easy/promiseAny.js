f318c506b7d845a24695790563dbeee07f316916
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-07-06 19:39:43 +0530

    done promiseAny problem

--- Asynchronous-Javascript/promises/easy/promiseAny.js
+++ Asynchronous-Javascript/promises/easy/promiseAny.js
@@ -1,8 +1,25 @@
 // Problem Description – promiseAny(promises)
 
-// You are required to implement a function named promiseAny that accepts an array of Promises. 
-// The function should return a new Promise that resolves immediately when any one of the input promises resolves successfully. 
+// You are required to implement a function named promiseAny that accepts an array of Promises.
+// The function should return a new Promise that resolves immediately when any one of the input promises resolves successfully.
 // If all the promises reject, the returned Promise should reject with an error.
-function promiseAny(promises) {}
+async function promiseAny(promises) {
+  return new Promise((resolve, reject) => {
+    let errCount = 0;
+    if (promises.length === 0) {
+      reject(new Error("Empty iterable"));
+    }
+    promises.forEach((ele, idx) => {
+      Promise.resolve(ele)
+        .then((data) => resolve(data))
+        .catch((err) => {
+          errCount += 1;
+          if (errCount === promises.length) {
+            reject(new Error("All promises were rejected"));
+          }
+        });
+    });
+  });
+}
 
 module.exports = promiseAny;
--- Asynchronous-Javascript/rough.js
+++ Asynchronous-Javascript/rough.js
@@ -1,37 +1,21 @@
-async function promiseAll(promises) {
-  return new Promise((res, rej) => {
-    let index = 0;
-    let results = [];
-    let completed = 0;
-    function next() {
-      if (completed === promises.length) {
-        // console.log(completed);
-        res(results);
-      }
-      while (index < promises.length) {
-        let currentIndex = index;
-        index += 1;
-        Promise.resolve(promises[currentIndex])
-          .then((data) => {
-            results[currentIndex] = data;
-            completed += 1;
-            next();
-          })
-          .catch((err) => {
-            rej(err);
-          });
-      }
-    }
-    next();
+async function promiseAny(promises) {
+  return new Promise((resolve, reject) => {
+    let errCount = 0;
+    promises.forEach((ele, idx) => {
+      Promise.resolve(ele)
+        .then((data) => resolve(data))
+        .catch((err) => {
+          errCount += 1;
+          if (errCount === promises.length) {
+            reject(new Error("All promises were rejected"));
+          }
+        });
+    });
   });
 }
 
 (async () => {
-  const p1 = new Promise((res) => setTimeout(() => res(1), 100));
-  const p2 = new Promise((res) => setTimeout(() => res(2), 50));
-  const p3 = new Promise((res) => setTimeout(() => res(3), 10));
-
-  const result = await promiseAll([p1, p2, p3]);
+  const result = await promiseAny([Promise.reject("err"), 42]);
   console.log(result);
 })();
 
